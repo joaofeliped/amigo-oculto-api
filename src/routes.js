10b58ba5147d17ba62057258cc8fb5594628a0fc
Felipe Marinho <felipmarinho@gmail.com>
2019-11-24 21:37:59 -0300

    Revert "Create user"

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,10 @@
 import { Router } from 'express';
 
 import AmigoOcultoController from './app/controller/AmigoOcultoController';
-import UserController from './app/controller/UserController';
 
 const routes = new Router();
 
 routes.get('/', (req, res) => res.json({ message: 'Hello world' }));
-
-routes.post('/users', UserController.store);
-routes.get('/users', UserController.index);
-routes.delete('/users/:id', UserController.delete);
 
 routes.post('/amigo-oculto', AmigoOcultoController.store);
 routes.get('/amigo-oculto', AmigoOcultoController.index);
